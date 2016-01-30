bf266f3a45dd740f9f6a8a0ede50cf0517ad48c0
Joshua Shaffer <joshuatshaffer@gmail.com>
2016-01-29 18:09:22 -0600

    Moved all player movement scripts to the namespace PlayerMovement.

--- Assets/Spider/SpiderLocomotion.cs
+++ Assets/Spider/SpiderLocomotion.cs
@@ -6,32 +6,34 @@ using System.Collections.Generic;
  * TODO
  * remove jitter when on rigidbody (probably due to position update timeing)
  */
-public class SpiderLocomotion : MonoBehaviour {
-	
-	[SerializeField] public Movement movement;
-	[SerializeField] public Head head;
-	[SerializeField] public Feet feet;
+namespace PlayerMovement {
+	public class SpiderLocomotion : MonoBehaviour {
 
-	private Ground ground;
-	private Rigidbody body;
+		[SerializeField] public Movement movement;
+		[SerializeField] public Head head;
+		[SerializeField] public Feet feet;
 
-	void Awake () {
-		ground = new Ground ();
-		body = GetComponent<Rigidbody> ();
+		private Ground ground;
+		private Rigidbody body;
 
-		feet.Init (body, ground);
-		ground.Init (body, feet);
-		head.Init (ground);
-		movement.Init (body, ground, head, transform);
-	}
+		void Awake () {
+			ground = new Ground ();
+			body = GetComponent<Rigidbody> ();
 
-	void Update () {
-		head.Update ();
-		movement.Update ();
-	}
+			feet.Init (body, ground);
+			ground.Init (body, feet);
+			head.Init (ground);
+			movement.Init (body, ground, head, transform);
+		}
 
-	void FixedUpdate () {
-		ground.Update ();
-		movement.FixedUpdate ();
+		void Update () {
+			head.Update ();
+			movement.Update ();
+		}
+
+		void FixedUpdate () {
+			ground.Update ();
+			movement.FixedUpdate ();
+		}
 	}
 }
